4118e7eabd1fd53226ef38778f7f334af4f556d7
yuseonjun <seonjun0906@gmail.com>
2023-09-13 16:13:03 +0900

    feat: 각 요일에 추천할 카테고리를 무작위로 정한다.

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,11 +1,13 @@
 package menu.domain;
 
+import java.util.Arrays;
+
 public enum Category {
-    JAPAN(1,"일식"),
-    KOREAN(2,"한식"),
-    CHINA(3,"중식"),
-    ASIAN(4,"아시안"),
-    WESTERN(5,"양식");
+    JAPAN(1, "일식"),
+    KOREAN(2, "한식"),
+    CHINA(3, "중식"),
+    ASIAN(4, "아시안"),
+    WESTERN(5, "양식");
 
     private final int value;
     private final String type;
@@ -13,5 +15,20 @@ public enum Category {
     Category(int value, String type) {
         this.value = value;
         this.type = type;
+    }
+
+    public static Category valueOf(int number) {
+        return Arrays.stream(Category.values())
+            .filter(category -> category.getValue() == number)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 값 입니다."));
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getType() {
+        return type;
     }
 }
---
+++ src/main/java/menu/utils/Util.java
@@ -0,0 +1,10 @@
+package menu.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Util {
+
+    public static int randomGenerator() {
+        return Randoms.pickNumberInRange(1, 5);
+    }
+}
