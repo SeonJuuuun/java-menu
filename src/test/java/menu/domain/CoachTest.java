b9d7c8040cc35393c08c6d5dd18d89d9c55c9b97
yuseonjun <seonjun0906@gmail.com>
2023-09-15 16:34:51 +0900

    refactor: CoachesTest 삭제

--- src/test/java/menu/domain/CoachTest.java
+++ src/test/java/menu/domain/CoachTest.java
@@ -10,13 +10,7 @@ class CoachTest {
     @DisplayName("코치의 이름의 범위를 벗어나면 예외가 발생한다.")
     @Test
     void checkCoachSize() throws Exception {
-        //given
-
-        //when
-
-        //then
         assertThatThrownBy(() -> new Coach("토"))
             .isInstanceOf(IllegalArgumentException.class);
     }
-
 }
